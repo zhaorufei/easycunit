e7d30ea3c1f618c109a4c1df197ad054c3f5d102
zhaorufei <zhaorufei@6f7ae9e1-4027-ce79-8af5-202386c9454e>
2010-09-25 09:37:02 +0000

    Change Runner to CURunner

--- trunk/build.sh
+++ trunk/build.sh
@@ -13,5 +13,5 @@ cl /c /Zi /Fd"ANSI_C_UnitTest_$vc_ver.pdb" ANSI_C_UnitTest.c
 lib /out:ANSI_C_UnitTest_$vc_ver.lib ANSI_C_UnitTest.obj
 cl /Zi simple_vc_gcc.c
 # Runner must be compiled with VC2010, uses tr1::regex
-# cl /EHsc /Zi /J Runner.cpp VC_ver_specific.cpp
-./runner simple_vc_gcc.exe
+# cl /EHsc /Zi /J CURunner.cpp VC_ver_specific.cpp
+./CUrunner simple_vc_gcc.exe
